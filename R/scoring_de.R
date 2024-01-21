96a0df4f9456acaf4842b838ed0dd0f27e71668d
Longda <longda.jiang.2015@gmail.com>
2024-01-21 00:49:36 -0500

    ordered p v2

--- R/scoring_de.R
+++ R/scoring_de.R
@@ -465,23 +465,23 @@ Run_wmvRegDE = function (object,
         rownames(res) = res$gene_ID
         
         # check if the user wants the full results or not 
+        idx_colnames1 = grep(pattern = "_weight", 
+                             x = colnames(res), 
+                             value = T)
+        idx_colnames2 = grep(pattern = "^log2FC", 
+                             x = colnames(res), 
+                             value = T)
+        # sort the results based on P and then beta
+        res = res[order(res[, tail(idx_colnames1, 1)], rev(abs(res[, tail(idx_colnames1, 1+length(splits))[1]])) ), ]
+        # 
+        if(length(idx_colnames2) == 1){
+            names(res)[which(names(res) == idx_colnames2)] = "log2FC"
+        }
+        
         if(isTRUE(full.results)){
             # nothing happens
         } else {
-            idx_colnames1 = grep(pattern = "_weight", 
-                                 x = colnames(res), 
-                                 value = T)
-            idx_colnames2 = grep(pattern = "^log2FC", 
-                                 x = colnames(res), 
-                                 value = T)
             res = res[, c("gene_ID", idx_colnames2, idx_colnames1, "DE_method")]
-            # sort the results based on P and then beta
-            
-            res = res[order(res[, tail(idx_colnames1, 1)], rev(abs(res[, tail(idx_colnames1, 1+length(splits))[1]])) ), ]
-            # 
-            if(length(idx_colnames2) == 1){
-                names(res)[which(names(res) == idx_colnames2)] = "log2FC"
-            }
         }
         
         all_res[[PRTB]] = res
